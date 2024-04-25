f12c634db0f5363ed4d7bc6d9fda998a6b57b7ef
FreddyTheHtoo <naywaisay2022@gmail.com>
2024-04-25 21:47:57 +0700

    Complete Dapper CRUD

--- NWSBDotNetCore.ConsoleApp/DapperExample.cs
+++ NWSBDotNetCore.ConsoleApp/DapperExample.cs
@@ -15,8 +15,12 @@ namespace NWSBDotNetCore.ConsoleApp
         public void Run()
         {
            /* Read()*/;
-            Edit(1);
+            //Edit(1);
             //Edit(11);
+            //Create("Titles", "Authors", "Contents");
+            //Update(1003,"Titles 2", "Authors 3", "Contents 3");
+            Delete(3);
+
         }
 
         private void Read()
@@ -54,6 +58,77 @@ namespace NWSBDotNetCore.ConsoleApp
             Console.WriteLine(item.BlogAuthor);
             Console.WriteLine(item.BlogContent);
             Console.WriteLine("-----------------------------");
+        }
+        private void Create(string title, string author, string content)
+        {
+            var item = new BloDto
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+        };
+
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+             ([BlogTitle]
+            ,[BlogAuthor]
+            ,[BlogContent])
+      VALUES (@BlogTitle
+                ,@BlogAuthor
+                 ,@BlogContent)";
+
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+        private void Update(int id, string title, string author, string content)
+        {
+            var item = new BloDto
+            {
+                BlogId  = id,
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+            };
+
+
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+     SET        [BlogTitle]=@BlogTitle
+            ,[BlogAuthor]=@BlogAuthor
+            ,[BlogContent]=@BlogContent
+    WHERE BlogId=@BlogId";
+
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Update Successful." : "Update Failed.";
+            Console.WriteLine(message);
+        }
+        
+        private void Delete(int id)
+        {
+            var item = new BloDto
+            {
+                BlogId = id,
+      
+            };
+
+
+            string query = @"DELETE FROM [dbo].[Tbl_Blog] WHERE BlogId=@BlogId";
+
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Delete Successful." : "Delete Failed.";
+            Console.WriteLine(message);
         }
     }
 }
