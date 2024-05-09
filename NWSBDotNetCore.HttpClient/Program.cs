709378912e8fe35378ffd25c4ec64639d30c68ee
FreddyTheHtoo <naywaisay2022@gmail.com>
2024-05-09 22:06:36 +0700

    [Complete] DreamDictionary

---
+++ NWSBDotNetCore.HttpClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+string str = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDto>(str);
+
+
+foreach(var header in model.BlogHeader)
+{
+    Console.WriteLine(header.BlogTitle);
+}
+Console.WriteLine(str);
+
+
+
+Console.ReadLine();
+
+public class MainDto
+{
+    public Blogheader[] BlogHeader { get; set; }
+    public Blogdetail[] BlogDetail { get; set; }
+}
+
+public class Blogheader
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+}
+
+public class Blogdetail
+{
+    public int BlogDetailId { get; set; }
+    public int BlogId { get; set; }
+    public string BlogContent { get; set; }
+}
+
---
+++ NWSBDotNetCore.RestApiWithNlayer/Features/Dream/DreamController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json; 
+
+namespace NWSBDotNetCore.RestApiWithNlayer.Features.Dream
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DreamController : ControllerBase
+    {
+        private async Task<DreamDto> GetDataAsyn()
+        {
+            string str = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<DreamDto>(str);
+            return model;
+        }
+
+        [HttpGet("{BlogHeader}")]
+        public async Task<IActionResult> Header()
+        {
+            var model= await GetDataAsyn();
+            return Ok(model.BlogHeader);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail()
+        {
+            var model = await GetDataAsyn();
+            return Ok(model.BlogDetail);
+        }
+    }
+
+
+    public class DreamDto
+    {
+        public Blogheader[] BlogHeader { get; set; }
+        public Blogdetail[] BlogDetail { get; set; }
+    }
+
+    public class Blogheader
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+    }
+
+    public class Blogdetail
+    {
+        public int BlogDetailId { get; set; }
+        public int BlogId { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+
+}
+
+
